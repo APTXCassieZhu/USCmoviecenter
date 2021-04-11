b1d24e7426eec2024d21ae814bd7d8865bb8f07e
ruiqi <ruiqi.zhu@stonybrook.edu>
2021-04-11 14:39:47 -0700

    first commit

---
+++ movieAPP/Shared/movieAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  movieAPPApp.swift
+//  Shared
+//
+//  Created by ruiqi on 2021/4/11.
+//
+
+import SwiftUI
+
+@main
+struct movieAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
