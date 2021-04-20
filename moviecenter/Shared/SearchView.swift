9238c4f450eaafbc96dd8822db94db5656658b9d
ruiqi <ruiqi.zhu@stonybrook.edu>
2021-04-20 12:21:39 -0700

    search bar & cancel button

--- moviecenter/Shared/SearchView.swift
+++ moviecenter/Shared/SearchView.swift
@@ -8,8 +8,79 @@
 import SwiftUI
 
 struct SearchView: View {
+    @State private var searchText : String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack{
+                SearchBar(text: $searchText)
+                ScrollView(.vertical){
+                    Text("lalala")
+                    Text("lalala")
+                    Text("lalala")
+                    Text("lalala")
+                    Text("lalala")
+                    Text("lalala")
+                }
+            }
+            .navigationTitle("Search")
+        }.navigationViewStyle(StackNavigationViewStyle())
+    }
+}
+
+struct SearchBar: UIViewRepresentable {
+
+    @Binding var text: String
+
+    class Coordinator: NSObject, UISearchBarDelegate {
+
+        @Binding var text: String
+
+        init(text: Binding<String>) {
+            _text = text
+        }
+
+        func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+            text = searchText
+            let cancelButtonAttributes = [NSAttributedString.Key.foregroundColor: UIColor.systemBlue]
+            UIBarButtonItem.appearance().setTitleTextAttributes(cancelButtonAttributes , for: .normal)
+            searchBar.showsCancelButton = true
+        }
+        
+        func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+           searchBar.setShowsCancelButton(true, animated: true)
+        }
+        
+        func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+            searchBar.setShowsCancelButton(false, animated: true)
+        }
+        
+        func searchBarSearchButtonClicked(_ searchBar: UISearchBar)
+        {
+            searchBar.endEditing(true)
+        }
+
+        func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+            searchBar.text = ""
+            searchBar.endEditing(true)
+        }
+    }
+
+    func makeCoordinator() -> SearchBar.Coordinator {
+        return Coordinator(text: $text)
+    }
+
+    func makeUIView(context: UIViewRepresentableContext<SearchBar>) -> UISearchBar {
+        let searchBar = UISearchBar(frame: .zero)
+        searchBar.delegate = context.coordinator
+        searchBar.searchBarStyle = .minimal
+        searchBar.placeholder = "Search Movies, Tvs..."
+        searchBar.autocapitalizationType = .none
+        return searchBar
+    }
+
+    func updateUIView(_ uiView: UISearchBar, context: UIViewRepresentableContext<SearchBar>) {
+        uiView.text = text
     }
 }
 
